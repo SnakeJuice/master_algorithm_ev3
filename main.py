370828b72cbbec0fed8686e3f594609c866ac14f
Cristian Anjari <93675228+SnakeJuice@users.noreply.github.com>
2024-01-05 15:58:23 -0300

    main.py upload

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
 #!/usr/bin/env pybricks-micropython
+##########################################################
+# Algoritmo Master que maneja la conexión entre robots   #
+#                                                        #
+# Autores:                                               #
+# - Cristian Anjari                                      #
+# - Marcos Medina                                        #
+#                                                        #
+# Para proyecto de tesis 2023                            #
+#                                                        #
+# Universidad de Santiago de Chile                       #
+# Facultad de Ciencia                                    #
+#                                                        #
+# Licenciatura en Ciencia de la Computación/             #
+# Analista en Computación Científica                     #
+#                                                        #
+# Santiago, Chile                                        #
+# 05/01/2024                                             #
+##########################################################
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.messaging import BluetoothMailboxServer, TextMailbox
 
-
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-
-# Create your objects here.
 ev3 = EV3Brick()
 
+server = BluetoothMailboxServer()
+mbox = TextMailbox('greeting', server)
 
-# Write your program here.
-ev3.speaker.beep()
+# The server must be started before the client!
+ev3.screen.print('waiting for connection...')
+server.wait_for_connection()
+ev3.screen.print('connected!')
+#robot.wait(300)
+ev3.screen.clear()
+
+# In this program, the server waits for the client to send the first message
+# and then sends a reply.
+
+mbox.wait()
+ev3.screen.print(mbox.read())
+mbox.send('inicia buscador')
+mbox.wait_new() 
+if(mbox.read() == 'buscador terminado'):
+    ev3.speaker.beep()
+
+
